4b156c07f3545506f3aeebec927cdad305a04bdf
Vincent <vince.shores@outlook.com>
2019-01-20 17:31:32 -0500

    Updated infiniteblocks script for piqueserver

--- src/infiniteblocks.py
+++ src/infiniteblocks.py
@@ -1,11 +1,11 @@
 """
-Automatically refills a players blocks similarly to how it is done in god mode, but without the invincibility
+Automatically refills a players blocks, similarly to how it is done in god mode, but without other effects
 """
 
-import commands
+from piqueserver.commands import command
 
 
-@commands.alias('ib')
+@command('infiniteblocks', 'ib')
 def infinite_blocks(connection):
     protocol = connection.protocol
     if connection not in protocol.players:
@@ -13,11 +13,10 @@ def infinite_blocks(connection):
     connection.god = False
     connection.infinite_blocks = not connection.infinite_blocks
     if connection.infinite_blocks:
-        message = '%s entered INFINITE BLOCKS MODE' % connection.name
+        message = '{} entered INFINITE BLOCKS MODE'.format(connection.name)
     else:
-        message = '%s returned to being a mere sightseer' % connection.name
+        message = '%s returned to being a mere sightseer'.format(connection.name)
     connection.protocol.send_chat(message, irc=True)
-commands.add(infinite_blocks)
 
 
 def apply_script(protocol, connection, config):
@@ -27,12 +26,13 @@ def apply_script(protocol, connection, config):
             self.infinite_blocks = False
 
         def on_hit(self, hit_amount, player, type_, grenade):
-            if player.infinite_blocks:
-                self.send_chat("You can't hurt %s! That player is in *Infinite Blocks Mode*" % player.name)
-                return False
-            if self.infinite_blocks:
-                self.send_chat("You can't hurt people while you are in *Infinite Blocks Mode*")
-                return False
+            if not player.invisible:
+                if player.infinite_blocks:
+                    self.send_chat("You can't hurt %s! That player is in *Infinite Blocks Mode*" % player.name)
+                    return False
+                if self.infinite_blocks:
+                    self.send_chat("You can't hurt people while you are in *Infinite Blocks Mode*")
+                    return False
             return connection.on_hit(self, hit_amount, player, type_, grenade)
 
         def on_block_build(self, x, y, z):
