814c09ae5e117541f7e9f12e3599587d072532b0
Vincent <vince.shores@outlook.com>
2019-01-20 19:46:12 -0500

    Protect automatically on map change

--- src/build.py
+++ src/build.py
@@ -3,6 +3,7 @@ Starts build mode for build server. Protects areas, changes team names and locks
 builder team.
 """
 from pyspades.common import coordinates
+from pyspades.constants import CTF_MODE
 from piqueserver.commands import command
 
 DONT_PROTECT = ['g7', 'g8', 'h7', ' h8']
@@ -10,20 +11,27 @@ DONT_PROTECT = ['g7', 'g8', 'h7', ' h8']
 
 @command('protectall')
 def protect_all(connection):
-    connection.protocol.init_protect()
+    connection.protocol.protect_all()
     connection.send_chat('Build areas protected')
 
 
 def apply_script(protocol, connection, config):
     class BuildProtocol(protocol):
+        game_mode = CTF_MODE
+
         def __init__(self, *args, **kwargs):
             protocol.__init__(self, *args, **kwargs)
             self.blue_team.locked = True
             self.blue_team.name = 'Builders'
             self.green_team.name = 'Others'
-            self.init_protect()
+            self.protect_all()
 
-        def init_protect(self):
+        def set_map(self, map_obj):
+            result = protocol.on_map_change(self, map_obj)
+            self.protect_all()
+            return result
+
+        def protect_all(self):
             self.protected = set()
             for letter in 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h':
                 for num in range(8):
