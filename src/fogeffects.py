6a473a980fb3f99bfcdeee50e1abd7b43b371e11
Vincent <vince.shores@outlook.com>
2019-01-22 02:15:52 -0500

    Fixed fog effects gradient

--- src/fogeffects.py
+++ src/fogeffects.py
@@ -1,10 +1,11 @@
 from twisted.internet.task import LoopingCall
 from twisted.internet.reactor import callLater, seconds
-from pyspades.color import interpolate_rgb
-from pyspades.server import fog_color, grenade_packet
-from pyspades.common import make_color, Vertex3
-from pyspades.world import Grenade
 from piqueserver.commands import command, get_player
+from pyspades.color import interpolate_rgb
+from pyspades.common import make_color, Vertex3
+from pyspades.contained import GrenadePacket
+from pyspades.server import fog_color
+from pyspades.world import Grenade
 from abc import abstractmethod, ABCMeta
 
 S_SPECTATING = "{player} is spectating"
@@ -24,7 +25,7 @@ ease_in = quadratic_inverse = lambda t: 1.0 - ((1.0 - t) ** 2)
 def lightning(connection, name=None):
     # And I will strike down upon thee with great vengeance and furious anger
     # those who would attempt to poison and destroy My brothers
-    
+
     protocol = connection.protocol
     if name:
         player = get_player(protocol, name)
@@ -33,10 +34,10 @@ def lightning(connection, name=None):
             return S_SPECTATING.format(player=player.name)
         if player.hp < 0:
             return S_NOT_ALIVE.format(player=player.name)
-        
+
         callLater(0.1, player.kill)
         callLater(0.1, create_explosion_effect_at_player, player)
-        
+
         message = S_LIGHTNING.format(player=player.name)
         protocol.send_chat(message, sender=player)
         player.send_chat(S_LIGHTNING_SELF)
@@ -51,7 +52,7 @@ def lightning(connection, name=None):
         FogHold(protocol, 1.0, (0, 0, 0)),
         FogGradient(protocol, 4.0, (0, 0, 0), protocol.get_fog_effects_off_color, ease_out)
     ]
-    protocol.set_fog_effects(effects)
+    protocol.run_fog_effects(effects)
 
 
 @command('fade')
@@ -61,11 +62,11 @@ def fade(connection, r, g, b, time=1.0):
     protocol = connection.protocol
     fade_time = time * 0.25
     effects = [
-        FogGradient(protocol, fade_time, protocol.fog_effects_off_color, color, ease_in),
+        FogGradient(protocol, fade_time, protocol.get_fog_effects_off_color, color, ease_in),
         FogHold(protocol, time, color),
         FogGradient(protocol, fade_time, color, protocol.get_fog_effects_off_color, ease_out)
     ]
-    protocol.set_fog_effects(effects)
+    protocol.run_fog_effects(effects)
 
 
 def create_explosion_effect_at_player(player):
@@ -74,6 +75,7 @@ def create_explosion_effect_at_player(player):
         return
     protocol = player.protocol
     grenade = protocol.world.create_object(Grenade, 0.0, obj.position, None, Vertex3(), None)
+    grenade_packet = GrenadePacket()
     grenade_packet.value = grenade.fuse
     grenade_packet.player_id = 32
     grenade_packet.position = grenade.position.get()
@@ -107,12 +109,12 @@ class FogHold(FogEffect):
         self.duration = duration
         self.color = self.wrap_if_necessary(color)
         self.call = None
-    
+
     def start(self):
         if not self.call or not self.call.active():
             self.call = callLater(self.duration, self.end)
-        self.protocol.fog_effects_on_color = self.color()
-    
+        self.protocol.set_fog_effects_on_color(self.color())
+
     def stop(self):
         if self.call and self.call.active():
             self.call.cancel()
@@ -123,9 +125,9 @@ class FogSimple(FogEffect):
     def __init__(self, protocol, color):
         FogEffect.__init__(self, protocol)
         self.color = self.wrap_if_necessary(color)
-    
+
     def start(self):
-        self.protocol.fog_effects_on_color = self.color()
+        self.protocol.set_fog_effects_on_color(self.color())
         self.end()
 
 
@@ -133,12 +135,12 @@ class FogGradient(FogEffect):
     def __init__(self, protocol, duration, begin, end, interpolator=linear):
         FogEffect.__init__(self, protocol)
         self.duration = duration
-        self.begin = self.wrap_if_necessary(begin)
-        self.end = self.wrap_if_necessary(end)
+        self.begin_color = self.wrap_if_necessary(begin)
+        self.end_color = self.wrap_if_necessary(end)
         self.interpolator = interpolator
         self.loop = LoopingCall(self.apply)
         self.complete = False
-    
+
     def start(self):
         if not self.loop.running:
             self.final_time = seconds() + self.duration
@@ -148,41 +150,27 @@ class FogGradient(FogEffect):
         if self.loop.running:
             self.loop.stop()
         self.loop = None
-    
+
     def get_color(self):
         t = 1.0 - (self.final_time - seconds()) / self.duration
         t = min(1.0, self.interpolator(t))
-        return interpolate_rgb(self.begin(), self.end(), t)
-    
+        return interpolate_rgb(self.begin_color(), self.end_color(), t)
+
     def apply(self):
         if self.complete:
             self.end()
             return
-        self.protocol.fog_effects_on_color = self.get_color()
+        self.protocol.set_fog_effects_on_color(self.get_color())
         self.complete = seconds() >= self.final_time
 
 
 def apply_script(protocol, connection, config):
     class FogEffectProtocol(protocol):
-        def __init__(self):
+        def __init__(self, *args, **kwargs):
             self._fog_effects = []
             self._fog_effects_off_color = protocol.fog_color
             self._fog_effects_on_color = protocol.fog_color
-
-        @property
-        def fog_effects_on_color(self):
-            return self._fog_effects_on_color
-
-        @fog_effects_on_color.setter
-        def fog_effects_on_color(self, color):
-            self._fog_effects_on_color = color
-            if self._fog_effects:
-                fog_color.color = make_color(*color)
-                self.protocol.send_contained(fog_color, save=True)
-
-        @property
-        def fog_effects_off_color(self):
-            return self._fog_effects_off_color
+            protocol.__init__(self, *args, **kwargs)
 
         @property
         def fog_color(self):
@@ -196,9 +184,18 @@ def apply_script(protocol, connection, config):
             if not self._fog_effects:
                 protocol.set_fog_color(self, color)
             self._fog_effects_off_color = color
-        
-        def set_fog_effects(self, effects):
-            self.clear_fog_effects()
+
+        def set_fog_effects_on_color(self, color):
+            self._fog_effects_on_color = color
+            if self._fog_effects:
+                fog_color.color = make_color(*color)
+                self.send_contained(fog_color, save=True)
+
+        def get_fog_effects_off_color(self):
+            return self._fog_effects_off_color
+
+        def run_fog_effects(self, effects):
+            self._clear_fog_effects()
             for effect in reversed(effects):
                 self._fog_effects.append(effect)
             if self._fog_effects:
@@ -209,17 +206,17 @@ def apply_script(protocol, connection, config):
             if self._fog_effects:
                 self._fog_effects[-1].start()
 
-        def clear_fog_effects(self):
+        def on_map_change(self, map_):
+            self._clear_fog_effects()
+            protocol.on_map_change(self, map_)
+
+        def on_map_leave(self):
+            self._clear_fog_effects()
+            protocol.on_map_leave(self)
+
+        def _clear_fog_effects(self):
             for fog_effect in self._fog_effects[:]:
                 fog_effect.stop()
             self._fog_effects = []
 
-        def on_map_change(self, map_):
-            self.clear_fog_effects()
-            protocol.on_map_change(self, map_)
-
-        def on_map_leave(self):
-            self.clear_fog_effects()
-            protocol.on_map_leave(self)
-    
     return FogEffectProtocol, connection
