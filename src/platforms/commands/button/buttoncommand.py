1cfb710d9b04a3fce646b3459b1f964caad4780d
Vincent <vince.shores@outlook.com>
2019-01-24 18:31:11 -0500

    Added info to command scripts

--- src/platforms/commands/button/buttoncommand.py
+++ src/platforms/commands/button/buttoncommand.py
@@ -1,3 +1,29 @@
+"""
+/b /button [command]
+    Starts button creation. Just build a block with the desired color.
+
+    A default button only has a Press trigger and no actions, so you'll need
+    to make it do something with /action.
+
+    Press the SNEAK key (V) while in any button mode to get information
+    about the button you're looking at.  Must be holding spade tool.
+
+    command:
+        new <label>
+            Starts button creation with a label already attached.
+        name <label>
+            Labels a button.  It's recommended you name things to avoid mistakes.
+        toggle
+            Disables or enables a button.  A disabled button does nothing.
+        cooldown <seconds>
+            The button will be able to be activated only once in the specified
+            interval. Default is 0.5 seconds.
+        destroy
+            Destroys a button, removing the block.
+        last
+            When you get asked to select a button, you can use this command
+            to automatically choose the last button you selected or created.
+"""
 import click
 
 from platforms import piqueargs
--- src/platforms/commands/trigger/triggercommand.py
+++ src/platforms/commands/trigger/triggercommand.py
@@ -1,3 +1,58 @@
+"""
+/t /trigger <command>
+    Triggers are what makes a button know when to act and when not to.
+
+    command:
+        add [not] <action>
+        set [not] <action>
+            Adds a trigger to the button. Set deletes all previous triggers first,
+            making the new trigger the only one.
+
+            Putting 'not' before the action makes it NEGATE the output.
+            If you want to make a button that activates when a player *leaves*
+            a zone, you could use "/trigger set not distance 5"
+
+            action:
+                press
+                    Fires when a player normally hits the button with the spade.
+                distance [radius=3]
+                    True when a player gets within <radius> blocks of the
+                    button (note: box distance, not sphere).
+                track [radius=3]
+                    Same as distance, but tracks one player and only one player.
+
+                    Useful for creating a button that requires a specific number
+                    of nearby players.
+                height <height>
+                    True when the platforms is exactly the specified height.
+        list
+            Lists the triggers present in the button you select.
+
+            Example:
+            "Triggers in 'mybutton': #0 player press OR #1 player distance=5 [CHECK]"
+
+            #0 and #1 are the trigger indexes to be used with /trigger del.
+            [CHECK] means the trigger currently yields true. The player in this
+            case is near the button, but hasn't pressed it.
+
+            This button uses OR logic, meaning that EITHER of these triggers
+            firing is enough to activate the button.
+        del <#|all>
+            Delete a single trigger in a button by specifying its index. Trigger
+            indexes can be looked up by using /trigger list.
+
+            Negative indexes can be used too: -1 is the last added trigger, -2 the
+            one before that, and so on.
+
+            Specifying 'all' instead of a number erases all the triggers.
+        logic <and|or>
+            "AND" will make the button activate when ALL its triggers yield true.
+            "OR" will make the button activate when ANY of its triggers fire.
+        quiet
+            Makes a button either become silent or resume playing animation and
+            sound when it activates.
+"""
+
 import click
 
 from platforms import piqueargs
