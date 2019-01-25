62d90863959e59113ec38b633e899f289c07d51d
Vincent <vince.shores@outlook.com>
2019-01-24 22:59:30 -0500

    Got rid of separate modules for each command because they dont need them

--- src/platforms/commands/commands.py
+++ src/platforms/commands/commands.py
@@ -1,18 +1,20 @@
-from piqueserver.commands import command
 from platforms.core.platformconnection import ACTION_RAY_LENGTH, ACTION_RAY_LENGTH_LONG
+
+
+from platforms import piqueargs
 
 S_SAVED = 'Platforms saved'
 S_REACH = 'You can now reach to buttons from from far away'
 S_NO_REACH = 'Button reach reverted to normal'
 
 
-@command('save')
+@piqueargs.command(usage=r'Usage: \save')
 def save(connection):
     connection.protocol.dump_platform_json()
     return S_SAVED
 
 
-@command('reach')
+@piqueargs.command(r'Usage: \reach')
 def reach(connection):
     if connection not in connection.protocol.players:
         raise ValueError()
