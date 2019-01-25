7370637d0df4df4519d6cc1aa23afc95251de37d
Vincent <vince.shores@outlook.com>
2019-01-24 20:52:34 -0500

    Reimplemented old buttoncommand functionality

--- src/platforms/commands/button/buttoncommand.py
+++ src/platforms/commands/button/buttoncommand.py
@@ -27,41 +27,77 @@
 import click
 
 from platforms import piqueargs
+from platforms.strings import S_EXIT_BLOCKING_STATE, S_NOT_POSITIVE, S_MINIMUM
+from platforms.states.buttoncommandstate import ButtonCommandState
+from platforms.states.newbuttonstate import NewButtonState
+from platforms.states.selectbuttonstate import SelectButtonState
 
 
-@piqueargs.group(usage="Usage: /button [new name destroy toggle cooldown last]")
-def button_command(connection):
-    pass
+MIN_COOLDOWN = 0.1  # seconds
+
+
+@piqueargs.group(usage="Usage: /button [new name destroy toggle cooldown last]", required=False)
+def button_command(connection, end=False):
+    if not end:
+        return
+
+    if connection not in connection.protocol.players:
+        raise ValueError()
+    state = connection.states.top()
+    if state and state.get_parent().name in ('new button', 'button command'):
+        connection.states.exit()  # cancel button creation
+    elif state and state.blocking:
+        return S_EXIT_BLOCKING_STATE.format(state=state.name)  # can't switch from a blocking mode
+    else:
+        connection.states.exit()
+        connection.states.enter(NewButtonState())
 
 
 @piqueargs.argument('label')
 @button_command.command(usage='Usage: /button new <label>')
 def new(connection, label):
-    return 'new {}'.format(label)
+    connection.states.exit()
+    connection.states.enter(NewButtonState(label))
 
 
 @piqueargs.argument('label')
 @button_command.command(usage='Usage: /button name <label>')
+@piqueargs.pass_obj
 def name(connection, label):
-    return 'name {}'.format(label)
+    state = ButtonCommandState('new')
+    state.label = label
+    push_command_state(connection, state)
 
 
-@piqueargs.argument('seconds', type=click.INT)
+@piqueargs.argument('seconds', type=click.FLOAT)
 @button_command.command(usage='Usage: /button cooldown <seconds>')
 def cooldown(connection, seconds):
-    return 'cooldown {}'.format(seconds)
+    if seconds < 0:
+        piqueargs.stop_parsing(S_NOT_POSITIVE.format(parameter='seconds'))
+    elif seconds < MIN_COOLDOWN:
+        piqueargs.stop_parsing(S_MINIMUM.format(paramer='seconds', value=MIN_COOLDOWN))
+    else:
+        state = ButtonCommandState('cooldown')
+        state.cooldown = seconds
+        push_command_state(connection, state)
 
 
 @button_command.command(usage='Usage: /button destroy')
 def destroy(connection):
-    return 'destroy'
+        push_command_state(connection, ButtonCommandState('destroy'))
 
 
 @button_command.command(usage='Usage: /button last')
 def last(connection):
-    return 'last'
+    state = connection.states.top()
+    if state and state.name == 'select button' and connection.previous_button:
+        state.button = connection.previous_button
+        connection.states.pop()
+    else:
+        push_command_state(connection, ButtonCommandState('last'))
 
 
-if __name__ == '__main__':
-    result = button_command.run('button_command', [])
-    print(result)
+def push_command_state(player, state):
+    player.states.exit()
+    player.states.push(state)
+    player.states.enter(SelectButtonState(state))
